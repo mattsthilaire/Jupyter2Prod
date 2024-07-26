24a29eae6af5f4b5472d6c9efc939b480f7278ba
MattStHilaire <mattsthilaire@ip-192-168-0-14.us-west-2.compute.internal>
2024-07-25 23:39:32 -0500

    Minimal viable API.

---
+++ src/main.py
@@ -0,0 +1,44 @@
+import io
+
+from fastapi import FastAPI, File, UploadFile
+import numpy as np
+import torch
+import torch.nn.functional as F
+from PIL import Image
+import uvicorn
+
+from .cfg import MODEL, IDX2LABEL
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello API!"}
+
+@app.post("/classify_image/")
+async def classify_image(file: UploadFile):
+    image = Image.open(io.BytesIO(await file.read()))
+    preprocessed_image = preprocess_image(image)
+    probs = run_classification(preprocessed_image)
+    return {"filename": file.filename, "image_size": preprocessed_image.shape, "probs": probs}
+
+def preprocess_image(image):
+    # convert PIL image to tensor and normalize
+    img_tensor = torch.tensor(np.array(image)) / 255.
+    
+    # return tensor that's shaped correctly for our neural network
+    return img_tensor.permute(2, 0, 1)[None, ...]
+
+def run_classification(image):
+    output = MODEL(image)
+    probs = F.softmax(output, dim=1)
+    
+    indices = torch.argsort(probs, descending=True).squeeze(0).tolist()
+    prob_list = probs[:, indices].squeeze(0, 1).tolist()
+    label_list = list(map(lambda x: IDX2LABEL[x], indices))
+    probs_list = {label:prob for label,prob in zip(label_list, prob_list)}
+    
+    return probs_list
+
+def start():
+    uvicorn.run("app")
